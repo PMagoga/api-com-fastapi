1852404e87dcb6c4b655fd5fac2b07eceb236c53
Pablo <pmagoga@gmail.com>
2026-09-14 14:53:40 -0300

    atualizando readm

--- auth_routes.py
+++ auth_routes.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
 
 from models import Usuario
 from dependencies import pegar_sessao
 from main import bcrypt_context
+from schemas import UsuarioSchema
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -12,15 +14,16 @@ async def home():
 
 
 @auth_router.post("/criar_conta")
-async def criar_conta(email: str, senha: str, nome: str, session = Depends(pegar_sessao)):
+async def criar_conta(usuario_schema: UsuarioSchema, session: Session = Depends(pegar_sessao)):
 
-    usuario = session.query(Usuario).filter(Usuario.email==email).first()
+    usuario = session.query(Usuario).filter(Usuario.email==usuario_schema.email).first()
     if usuario:
         # já existe um usuário
-        return {"Mensagem": "Usuário já cadastrado"}
+        return HTTPException(status_code=400, detail="Mensagem: Usuário já cadastrado")
     else:
-        senha_criptografada = bcrypt_context.hash(senha)
-        novo_usuario = Usuario(nome, email, senha_criptografada)
+        senha_criptografada = bcrypt_context.hash(usuario_schema.senha)
+        novo_usuario = Usuario(usuario_schema.nome, usuario_schema.email, senha_criptografada,
+                               usuario_schema.ativo, usuario_schema.admin)
         session.add(novo_usuario)
         session.commit()
         return {"Mensagem": "Usuário cadastrado com sucesso"}
