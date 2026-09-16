90700ea9ac1fdf63853fd09e2da21886d00432eb
Pablo <pmagoga@gmail.com>
2026-09-16 15:55:56 -0300

    adicionando jwt

--- auth_routes.py
+++ auth_routes.py
@@ -1,17 +1,27 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-
 from models import Usuario
 from dependencies import pegar_sessao
-from main import bcrypt_context
+from main import bcrypt_context, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY
 from schemas import LoginSchema, UsuarioSchema
+from jose import jwt, JWTError
+from datetime import datetime, timedelta, timezone
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
-def criar_token(id_usuario):
-    token = f"sdafljkfknaskfja{id_usuario}"
-    return token
+def criar_token(id_usuario, duracao_token=ACCESS_TOKEN_EXPIRE_MINUTES):
+    data_expiracao = datetime.now(timezone.utc) + timedelta(minutes=duracao_token)
+    dic_info = {"sub": id_usuario, "expiration": data_expiracao}
+    jwt_codificado = jwt.encode(dic_info, SECRET_KEY, algorithm=ALGORITHM)
+    return jwt_codificado
 
+def autenticar_usuario(email, senha, session):
+    usuario = session.query(Usuario).filter(Usuario.email==email).first()
+    if not usuario:
+        return False
+    elif not bcrypt_context.verify(senha, usuario.senha):
+        return False
+    return usuario
 
 @auth_router.get("/")
 async def home():
@@ -36,9 +46,10 @@ async def criar_conta(usuario_schema: UsuarioSchema, session: Session = Depends(
 # login -> email + senha -> toke JWT
 @auth_router.post("/login")
 async def login(login_schema: LoginSchema, session: Session = Depends(pegar_sessao)):
-    usuario = session.query(Usuario).filter(Usuario.email==login_schema.email).first()
-    if usuario:
-        raise HTTPException(status_code=400, detail="Usuário não cadastrado")
+    usuario = autenticar_usuario(login_schema.email, login_schema.senha, session)
+    if not usuario:
+        raise HTTPException(status_code=400, detail="Usuário não cadastrado, ou credenciais inválidas")
     else:
         access_token = criar_token(usuario.id)
+        refres_token = criar_token(usuario.id)
         return {"access_token": access_token, "token_type": "bearer"}
